eea0b317213c895ae76f125a95e593ef642b6bd5
WaterWan <598127577@qq.com>
2017-06-12 22:38:16 +0800

    code

---
+++ code/usaco/Problem015_skidesign/skidesign.cpp
@@ -0,0 +1,14 @@
+/**
+ID: njuwz151
+TASK: skidesign
+LANG: C++
+*/
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	freopen("skidesign.in", "r", stdin);
+	freopen("skidesign.out", "w", stdout);
+} 
