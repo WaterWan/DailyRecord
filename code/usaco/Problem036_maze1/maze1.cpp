a834dccbbafb4798dd251497013be093854f971f
WaterWan <598127577@qq.com>
2017-08-08 09:03:14 +0800

    ..

--- code/usaco/Problem036_maze1/maze1.cpp
+++ code/usaco/Problem036_maze1/maze1.cpp
@@ -5,20 +5,27 @@ LANG: C++11
 **/ 
 #include <stdio.h>
 #include <cstring>
+#include <queue>
+#include <utility>
+#include <algorithm>
+using namespace std;
 const int max_w = 45;
 const int max_h = 105;
 int w, h;
-char maze[2 * max_w][2 * max_h];
-bool can_visit[max_w][max_h][4];
+char maze[2 * max_h][2 * max_w];
+bool can_visit[max_h][max_w][4];
 int dx[] = {-1, 0, 1, 0};
 int dy[] = {0, 1, 0, -1}; 
 int exit_x[2];
 int exit_y[2];
 int exit_count = 0;
 char unused;
+int steps1[max_h][max_w];
+int steps2[max_h][max_w];
+int steps[max_h][max_w];
 int main() {
 	freopen("maze1.in", "r", stdin);
-//	freopen("maze1.out", "w", stdout);
+	freopen("maze1.out", "w", stdout);
 	scanf("%d%d", &w, &h);
 	scanf("%c", &unused);
 	for(int i = 0; i < 2 * h + 2; i++) {
@@ -68,6 +75,65 @@ int main() {
 			exit_count++;
 		}
 	}
-
+	memset(steps1, 0, sizeof steps1);
+	steps1[exit_x[0]][exit_y[0]] = 1;
+	queue<pair<int, int> > q1;
+	q1.push(make_pair(exit_x[0], exit_y[0]));
+	while(!q1.empty()) {
+		auto temp = q1.front();
+		for(int i = 0; i < 4; i++) {
+			if(temp.first + dx[i] < 0 || temp.first + dx[i] >= h) {
+				continue;
+			}
+			if(temp.second + dy[i] < 0 || temp.second + dy[i] >= w) {
+				continue;
+			}
+			if(!can_visit[temp.first][temp.second][i]) {
+				continue;
+			}
+			if(steps1[temp.first + dx[i]][temp.second + dy[i]]) {
+				continue;
+			}
+			steps1[temp.first + dx[i]][temp.second + dy[i]] = steps1[temp.first][temp.second] + 1;
+			q1.push(make_pair(temp.first + dx[i], temp.second + dy[i]));
+		}
+		q1.pop();
+	}
+	
+	memset(steps2, 0, sizeof steps2);
+	steps2[exit_x[1]][exit_y[1]] = 1;
+	queue<pair<int, int> > q2;
+	q2.push(make_pair(exit_x[1], exit_y[1]));
+	while(!q2.empty()) {
+		auto temp = q2.front();
+		for(int i = 0; i < 4; i++) {
+			if(temp.first + dx[i] < 0 || temp.first + dx[i] >= h) {
+				continue;
+			}
+			if(temp.second + dy[i] < 0 || temp.second + dy[i] >= w) {
+				continue;
+			}
+			if(!can_visit[temp.first][temp.second][i]) {
+				continue;
+			}
+			if(steps2[temp.first + dx[i]][temp.second + dy[i]]) {
+				continue;
+			}
+			steps2[temp.first + dx[i]][temp.second + dy[i]] = steps2[temp.first][temp.second] + 1;
+			q2.push(make_pair(temp.first + dx[i], temp.second + dy[i]));
+		}
+		q2.pop();
+	}
+	
+	memset(steps, 0, sizeof steps);
+	int max_step = -1;
+	for(int i = 0; i < h; i++) {
+		for(int j = 0; j < w; j++) {
+			steps[i][j] = min(steps1[i][j], steps2[i][j]);
+			max_step = max(max_step, steps[i][j]);
+		}
+	}
+	printf("%d\n", max_step);
+	return 0;
 	
 }
