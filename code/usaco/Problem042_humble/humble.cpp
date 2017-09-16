ff622085b636cb8db8449e81a825e50d457cf39d
WaterWan <598127577@qq.com>
2017-09-16 14:44:01 +0800

    ..

--- code/usaco/Problem042_humble/humble.cpp
+++ code/usaco/Problem042_humble/humble.cpp
@@ -3,10 +3,29 @@ ID: Wan Zhong
 TASK: humble
 LANG: C++11
 **/ 
-#include <iostream>
-
+#include<iostream>
+#include<cstdio>
 using namespace std;
-
-int main() {
-	
+typedef long long ll;
+const ll N=1e5+5,K=105,INF=1e10;
+ll n,k,ans[N],a[K],s[N];
+int main(){
+	freopen("humble.in", "r", stdin);
+	freopen("humble.out", "w", stdout); 
+    cin >> k >> n;
+    for(ll i = 1; i <= k; i++) {
+    	scanf("%d",&a[i]);
+	}
+    ans[0] = 1;
+    for(ll i = 1;i <= n; i++) {
+        ll now = INF;
+        for(ll j = 1; j <= k; j++) {
+            while(a[j]*ans[s[j]] <= ans[i-1]) {
+            	 s[j]++;//Î¬»¤s[j]
+            }
+            now = min(now, a[j] * ans[s[j]]);//¸üÐÂf[i]
+        }
+        ans[i] = now;
+    }
+    cout << ans[n] << endl;
 }
